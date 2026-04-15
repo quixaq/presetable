e178355b569c4e74ac790fdb9058fd1f9b51b988
Quixaq <quixaq@tutamail.com>
2026-04-15 19:21:33 +0200

    feat: implement cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Preset { id: Option<usize> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Preset { id } => match id {
+            Some(_id) => todo!("Choose the provided preset"),
+            None => todo!("Interactive prompt for preset"),
+        },
+    }
 }
